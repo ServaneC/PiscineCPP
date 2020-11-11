294086d964ef116a9b09cf5a55f463f0d021cb43
Servane Chêne <servanechene@macbook-pro-de-servane.home>
2020-11-11 12:24:44 +0100

    ex07

--- Cpp01/ex07/srcs/error.cpp
+++ Cpp01/ex07/srcs/error.cpp
@@ -6,22 +6,15 @@
 /*   By: schene <schene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 21:43:01 by schene            #+#    #+#             */
-/*   Updated: 2020/11/11 11:16:58 by schene           ###   ########.fr       */
+/*   Updated: 2020/11/11 12:23:48 by schene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "replace.hpp"
 
-int		print_error(int error)
+int		print_error(std::string  error)
 {
-	if (error == ER_AC)
-		std::cout << ERROR << "Replace need a path to a file and two NOT empty strings." << ENDL;
-	else if (error == ER_OPEN)
-		std::cerr << ERROR << "Error: the file couldn't be opened, please verify the path." << ENDL;
-	else if (error == ER_CREATE)
-		std::cerr << ERROR << "Error: the file couldn't be created." << ENDL;
-	else if (error == ER_EMPTY)
-		std::cerr << ERROR << "Error: one or both of the string are empty." << ENDL;
+	std::cout << ERROR << error << ENDL;
 	return (1);
 	
 }
--- Cpp01/ex07/srcs/replace.hpp
+++ Cpp01/ex07/srcs/replace.hpp
@@ -6,7 +6,7 @@
 /*   By: schene <schene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 21:43:22 by schene            #+#    #+#             */
-/*   Updated: 2020/11/11 11:37:29 by schene           ###   ########.fr       */
+/*   Updated: 2020/11/11 12:23:42 by schene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 # include <iostream>
 # include <fstream>
 
-# define ER_AC 0
-# define ER_OPEN 1
-# define ER_CREATE 2
-# define ER_EMPTY 3
+# define ER_AC "Replace need a path to a file and two NOT empty strings."
+# define ER_OPEN "The file couldn't be opened, please verify the path."
+# define ER_CREATE "The file couldn't be created."
+# define ER_EMPTY "One or both of the string are empty."
 # define ERROR "\033[0;31m❌ Error: "
 # define SUCCESS "\033[0;32m✅ "
 # define ENDL "\033[0m\n"
 
-int		print_error(int error);
+int		print_error(std::string  error);
 
 #endif
