0059313ce447ebb3f6da7344e531209aae9a8b5f
Servane Chêne <servanechene@MacBook-Pro-de-Servane.local>
2020-12-07 15:46:55 +0100

    ex01

---
+++ Cpp06/ex01/srcs/main.cpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schene <schene@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/07 12:19:17 by schene            #+#    #+#             */
+/*   Updated: 2020/12/07 15:46:13 by schene           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+struct Data
+{
+	std::string s1;
+	int			n;
+	std::string s2;
+};
+
+char	random_char(void)
+{
+	std::string alphnum = "abcdefghijklmnopqrstuvw1234567890";
+	int		index = std::rand() % alphnum.size();
+
+	return(alphnum[index]);	
+}
+
+void * 	serialize(void)
+{
+	char	*raw = new char[sizeof(char) * (8 * 2) + sizeof(int)];
+
+	for (int i = 0; i < 8; i++)
+		raw[i] = random_char();
+	*reinterpret_cast<int*>(&raw[8]) = std::rand() % INT_MAX;
+	for (int i = 12; i < 20; i++)
+		raw[i] = random_char();	
+	return (raw);
+}
+
+Data * deserialize(void * raw)
+{
+	char *raw_bytes = reinterpret_cast<char *>(raw);
+	Data *data = new Data;
+	
+	for (int i = 0; i < 8; i++)
+		data->s1.append(1, raw_bytes[i]);
+	data->n = *reinterpret_cast<int*>(&raw_bytes[8]);
+	for (int i = 12; i < 20; i++)
+		data->s2.append(1, raw_bytes[i]);
+	return data;
+}
+
+int main()
+{
+	std::srand(std::time(NULL));
+	void *raw = serialize();
+	std::cout << "adress: " << raw << std::endl;
+	Data *data = deserialize(raw);
+	std::cout << data->s1 << std::endl;
+	std::cout << data->n << std::endl;
+	std::cout << data->s2 << std::endl;
+	delete data;
+	operator delete (raw);
+	return 0;
+}
